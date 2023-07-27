40b123e7fd635a02324367a3a034eb031593cf5e
Tyson Cashaw <Tyson@Mikas-MacBook-Pro-193.local>
2023-07-27 11:14:15 -0400

    added modified car.js

--- car.js
+++ car.js
@@ -5,6 +5,18 @@ function calculation(event)
     let totalMonthlyPayment;
     
     const priceOfVehicle = document.getElementById("priceOfVehicle").value;
-    document.getElementById("totalpayment").innerHTML = "$" + priceOfVehicle;
+    const interestRate = document.getElementById("interestRate").value;
+    const loanTerm = document.getElementById("loanTerm").value;
+    const downPayment = document.getElementById("downPayment").value;
+
+
+    // let finalPrice = priceOfVehicle - downPayment;
+    // totalInterest = finalPrice * interestRate;
+    // totalMonthlyPayment = (finalPrice + totalInterest) / loanTerm
+    
+    
+    document.getElementById("totalMonthyPayment").innerHTML = "$" + priceOfVehicle;
+
+    
     event.preventDefault();
 }
