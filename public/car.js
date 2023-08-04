65cd3755118317da3750f9f58ee8fa878244be43
Tyson Cashaw <tysoncashaw@Tysons-MBP.lan>
2023-08-04 14:03:27 -0400

    updated validation

--- public/car.js
+++ public/car.js
@@ -2,22 +2,28 @@
 function calculation(event) 
 {
     let totalInterest; 
-    let totalMonthlyPayment;
     
     const priceOfVehicle = document.getElementById("priceOfVehicle").value;
     const interestRate = document.getElementById("interestRate").value;
     const loanTerm = document.getElementById("loanTerm").value;
     const downPayment = document.getElementById("downPayment").value;
 
+    if(parseFloat(interestRate) <= 0){
+        var errorMsg=  document.getElementById("totalpayment");
+        errorMsg.innerHTML = "Interest rate can not be 0%";
+        errorMsg.setAttribute("class", "text-danger");
+        return;
+    }
 
-    let finalPrice = priceOfVehicle - downPayment;
+    let finalPrice = parseFloat(priceOfVehicle) - parseFloat(downPayment);
     totalInterest =  parseFloat(interestRate)/1200;
     
     let calc = ((totalInterest + (totalInterest / (Math.pow((1 + totalInterest), loanTerm) -1))) * finalPrice).toFixed(2);
 
     
-    document.getElementById("totalpayment").innerHTML = "$" + calc;
-
+    var total=document.getElementById("totalpayment");
+    total.innerHTML = "$" + parseFloat(calc);
+    
     
     event.preventDefault();
 }
