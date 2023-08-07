02dd99ba9d39d9bca8e1f97be63cb9da41876307
Tyson Cashaw <tysoncashaw@Tysons-MBP.lan>
2023-08-07 12:49:30 -0400

    Made an animation for the dollar amount

--- public/car.js
+++ public/car.js
@@ -22,8 +22,23 @@ function calculation(event)
 
     
     var total=document.getElementById("totalpayment");
+    fadeIn(total);
     total.innerHTML = "$" + parseFloat(calc);
     
+
     
     event.preventDefault();
+}
+
+function fadeIn(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 50);
 }
